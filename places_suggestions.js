f8140d3fe525a3e9377f2f14f2c17592a0236d16
tiffanyc1107 <p938196@gmail.com>
2017-10-22 05:14:49 -0700

    places_suggestions update

--- places_suggestions.js
+++ places_suggestions.js
@@ -1,24 +1,27 @@
-var GoogleMapsAPI = require('googlemaps');
-var gmAPI = new GoogleMapsAPI();
-//var params = {lat, lon}
+
 module.exports = function(controller) 
 {
-  controller.hears('place','message_received', function(bot, message) 
+  controller.hears(['place'],['message_received'], function(bot, message) 
   {
     bot.reply(message,
-    {   text:"MeetUp here!",
-        quick_replies:[{
-          "content_type": application/json
-          "https://www.google.com/maps/search/movie+theaters/@28.354177,-81.6731525,11z/data=!3m1!4b1",      // asks user to send location
-        }]
-      })
-    })
-  };
-"Content-Type: application/json" -d '{
-  "recipient": {
-    "id": "<PSID>"
-  },
-  "message": {
-    "text": "hello, world!"
-  }
-}' "https://graph.facebook.com/v2.6/me/messages?access_token=<PAGE_ACCESS_TOKEN>"
+    {  
+      attachment: {
+            'type':'template',
+            'payload':{
+                 'template_type':'generic',
+                 'elements':[
+                   {
+                     'title':'MeetUp here!',
+                     'image_url':'http://www.jqueryscript.net/images/Show-Nearby-Places-jQuery-Google-Maps-WhatsNearby.jpg',
+                     'buttons':[
+                       {
+                         'type':'web_url',
+                         'url':'https://www.google.com/maps/search/movie+theaters/@28.354177,-81.6731525,11z/data=!3m1!4b1',
+                         'title':'View Map'
+                       }]
+                   }]
+            }
+    }    
+  });
+});
+}
