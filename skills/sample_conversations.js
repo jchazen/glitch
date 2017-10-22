f61026fba17adbe98613c5f07320d50382a9777c
mc1134 <j.michael.chen@gmail.com>
2017-10-22 11:46:38 -0700

    Fixing bugs 11

--- skills/sample_conversations.js
+++ skills/sample_conversations.js
@@ -266,6 +266,38 @@ module.exports = function(controller) {
       });
     });
   });
+
+  // listen for the phrase `shirt` and reply back with structured messages
+  // containing images, links and action buttons
+  controller.hears(['let\'s go!', 'leggo'],'message_received',function(bot, message){
+    //start conversation after hearing key word
+    bot.startConversation(message, function(err, convo){
+      convo.addQuestion({
+        text:"Where are you?",
+        quick_replies:[{
+          "content_type":"location",      // asks user to send location
+        }]
+      }, function(response, convo){
+         
+        //parsing latitude and longitude based on user location
+        var obj = {
+          lat: response.attachments[0].payload.coordinates.lat,
+          lon: response.attachments[0].payload.coordinates.long
+        }
+        console.log("Looks like user " + message.user + " is at: "+ JSON.stringify(obj));
+
+        //if message.user found in file, delete
+        var o = db.get(message.user)
+        if(o !== null) db.delete(message.user);
+        //writing data to json file
+        db.save(message.user, JSON.stringify(obj));
+        
+        bot.reply(message, "You are at "+ obj.lat + " " +obj.lon);
+        bot.reply(message, "Let's explore!")
+        
+      },{key: 'location'}, 'default');
+    });
+  });
 }
 
 // A function that checks if the string follows a time format.
