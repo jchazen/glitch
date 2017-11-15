e0d2151c9d95d7d086f1d33957388066bb2e878c
Michael Chen <j.michael.chen@gmail.com>
2017-11-15 15:52:14 -0800

    Moved the new help block up

--- skills/help.js
+++ skills/help.js
@@ -6,20 +6,28 @@ module.exports = function(controller) {
     // jump to that subthread (if it exists)
     controller.studio.before('help', function(convo, next) {
 
+        convo.say('New user? Forgot how to talk to me? Here are some things that might jog your memory: \n' +
+                'Ask me to \"meet\" if you want to plan a group meeting! \n' +
+                'Tell me about a \"fruit\"! \n' +
+                'Want to \"find\" your friend\'s group meeting? \n' +
+                'Need to confirm your location? Then \"let\'s go\"! \n' +
+                'Just want to say \"hello\"?');
+        
         // is there a parameter on the help command?
         // if so, change topic.
         if (matches = convo.source_message.text.match(/^help (.*)/i)) {
             if (convo.hasThread(matches[1])) {
                 convo.gotoThread(matches[1]);
             }
-        } else {
+        } /* else {
             convo.say('New user? Forgot how to talk to me? Here are some things that might jog your memory: \n' +
                 'Ask me to \"meet\" if you want to plan a group meeting! \n' +
                 'Tell me about a \"fruit\"! \n' +
                 'Want to \"find\" your friend\'s group meeting? \n' +
                 'Need to confirm your location? Then \"let\'s go\"! \n' +
                 'Just want to say \"hello\"?');
-        }
+        } */
+        // The above block is moved before the prior if statement
 
         next();
 
